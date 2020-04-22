8a75c3a013fe0687472699b59c2131f7ab887c20
sedationh <2493264676@qq.com>
2020-04-22 16:00:57 +0800

    :tada:项目初始化&views&router&icon

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '../views/home'
+import User from '../views/user'
+import Pin from '../views/pin'
+import Search from '../views/search'
+import Book from '../views/book'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  }, {
+    path: '/home',
+    component: Home,
+  }, {
+    path: '/user',
+    component: User,
+  }, {
+    path: '/search',
+    component: Search,
+  }, {
+    path: '/books',
+    component: Book,
+  }, {
+    path: '/pin',
+    component: Pin
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
